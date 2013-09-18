77a384946eaa2791122dead8a44075588243cc08
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-17 21:31:04 -0700

    Add form for new users and links to signin on home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    # save the current user in the session 
+  end
+
+  def destroy
+    # remove the current user id from the session
+  end
 end
