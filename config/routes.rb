cddc9eb08c77842c723f0aa3730041d781762d2e
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-17 20:37:54 -0700

    Add basic user controller to signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 UserRegistration::Application.routes.draw do
+  resources :users
+
   get "/home" => "static_pages#home"
   get "/secret" => "static_pages#secret"
+
+  get "/signup" => "users#new", as: :signup
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
