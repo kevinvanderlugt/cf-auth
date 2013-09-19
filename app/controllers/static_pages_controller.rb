360e80f9cec2a398929635f89732b4fb26ca57da
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-18 22:10:11 -0700

    Require user to be logged in to view secret page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user, only: :secret
+
   def home
   end
 
   def secret
   end
+
+  private
+    def authenticate_user
+      unless current_user?
+        flash[:alert] = "Please signin to access this page"
+        redirect_to signin_path
+      end
+    end
 end
